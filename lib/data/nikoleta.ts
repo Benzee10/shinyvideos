fe5400ed600291d93af376cebb2d1f7ae14de1fb
Benzee10 <168162718+Benzee10@users.noreply.github.com>
2025-08-03 20:48:28 +0100

    Create nikoleta.ts

---
+++ lib/data/nikoleta.ts
@@ -0,0 +1,15 @@
+{
+    "slug": "blonde-babe-nikoletta-is-lookin-fine-as-she-strips-down-showin-off-her-hot-bod-slowly-she-moves-south-to-her-privates-and-gets-to-work-rubbin-her-juicy-bud-with-intense-vigor",
+    "title": "Blonde babe Nikoletta is lookin' fine as she strips down, showin' off her hot bod. Slowly, she moves south to her privates and gets to work, rubbin' her juicy bud with intense vigor.",
+    "uploadDate": "2025-08-03",
+    "thumbnail": "https://i.postimg.cc/CMBqRgYm/8ddd7dd5a57d2679f49d75d07e54329c-mp4-4-1280.jpg",
+    "videoUrl": "https://www.xerotica.com/embed/55753",
+    "duration": "02:57",
+    "tags": [
+        "Nikoleta",
+        "Masturbation",
+        "Blonde",
+        "Femjoy"
+    ],
+    "description": "."
+}
