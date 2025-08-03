45f5d84dcfd7130c5bf2b0b4732612e545a5f2a8
Benzee10 <168162718+Benzee10@users.noreply.github.com>
2025-08-03 01:24:49 +0100

    Update user-uploads.ts

--- lib/data/user-uploads.ts
+++ lib/data/user-uploads.ts
@@ -9,5 +9,21 @@ export default [
         duration: '23:45',
         tags: ['User Upload', 'Misc'],
         description: "This video was uploaded by a user."
-    }
+    },
+{
+    "slug": "alluring-well-endowed-babe-strips-on-the-floor-exposing-her-alluring-slender-figure-with-massive-natural-boobs-and-masturbates",
+    "title": "Alluring well-endowed babe strips on the floor exposing her alluring slender figure with massive natural boobs and masturbates",
+    "uploadDate": "2025-08-03",
+    "thumbnail": "https://i.postimg.cc/x1tbV43c/341bd6e23da75fcc2275ea802a920007-mp4-8-1280.jpg",
+    "videoUrl": "https://www.xerotica.com/embed/54930",
+    "duration": "04:06",
+    "tags": [
+        "Sasha E",
+        "Brunette",
+        "Masturbation",
+        "Big Boobs",
+        "The Life Erotic"
+    ],
+    "description": "The Life Erotic"
+}
 ];
