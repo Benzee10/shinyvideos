5736a371567aa6e8061aaf4273f2d229dea9c8aa
Benzee10 <168162718+Benzee10@users.noreply.github.com>
2025-08-03 19:58:13 +0100

    Create melissa-stratton.ts

---
+++ lib/data/melissa-stratton.ts
@@ -0,0 +1,17 @@
+export default [
+    {
+    "slug": "horny-housewife-showcases-her-culinary-prowess-as-she-simultaneously-displays-her-sexual-allure-by-flaunting-her",
+    "title": "Horny housewife showcases her culinary prowess as she simultaneously displays her sexual allure by flaunting her",
+    "uploadDate": "2025-08-03",
+    "thumbnail": "https://i.postimg.cc/zBk2YrQw/cd7a2e2a9d1c66b57dd27e464aed04fe-mp4-6-1280.jpg",
+    "videoUrl": "https://www.xerotica.com/embed/55571",
+    "duration": "05:58",
+    "tags": [
+        "Melissa Stratton",
+        "Tattoo",
+        "Big Boobs",
+        "Playboy Plus"
+    ],
+    "description": "."
+}
+];
